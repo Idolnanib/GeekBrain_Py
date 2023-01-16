a5669c5267b3b743952ab9d245c2d8e5237d04d0
Zhirickii <vovanzhirickii@mail.ru>
2023-01-16 14:56:25 +0300

    Проработаны задачи урока

--- lesson_3_functions_dictionaries/Lesson3.py
+++ lesson_3_functions_dictionaries/Lesson3.py
@@ -4,17 +4,17 @@
 # >>> num_translate("eight")
 # "восемь"
 # Если перевод сделать невозможно, вернуть None. Подумайте, как и где лучше хранить информацию, необходимую для перевода: какой тип данных выбрать, в теле функции или снаружи.
-from translate import Translator
-
-def num_translate(number):
-    translator = Translator(to_lang='Russian')
-    translation = translator.translate(number)
-    if number == 'one':
-        translation = 'один'
-    return translation
-
-print(num_translate('One'))
-print(num_translate('Eight'))
+# from translate import Translator
+#
+# def num_translate(number):
+#     translator = Translator(to_lang='Russian')
+#     translation = translator.translate(number)
+#     if number == 'one':
+#         translation = 'один'
+#     return translation
+#
+# print(num_translate('One'))
+# print(num_translate('Eight'))
 
 
 # 2. * (вместо задачи 1) Доработать предыдущую функцию в num_translate_adv(): реализовать корректную работу с числительными, начинающимися с заглавной буквы — результат тоже должен быть с заглавной. Например:
@@ -22,6 +22,19 @@ print(num_translate('Eight'))
 # "Один"
 # >>> num_translate_adv("two")
 # "два"
+
+# from translate import Translator
+#
+# def num_translate(number):
+#     translator = Translator(to_lang='Russian')
+#     translation = translator.translate(number)
+#     if number == 'one':
+#         translation = 'один'
+#     return translation
+#
+# print(num_translate('One'))
+# print(num_translate('Eight'))
+
 # 3. Написать функцию thesaurus(), принимающую в качестве аргументов имена сотрудников и возвращающую словарь, в котором ключи — первые буквы имён, а значения — списки, содержащие имена, начинающиеся с соответствующей буквы. Например:
 # >>>  thesaurus("Иван", "Мария", "Петр", "Илья")
 # {
@@ -29,6 +42,21 @@ print(num_translate('Eight'))
 #     "М": ["Мария"], "П": ["Петр"]
 # }
 # Подумайте: полезен ли будет вам оператор распаковки? Как поступить, если потребуется сортировка по ключам? Можно ли использовать словарь в этом случае?
+
+# list_of_names = ["Иван", "Мария", "Петр", "Илья"]
+#
+# def thesaurus(list_of_names):
+#     vocabulary = {}
+#     vocabulary = {}
+#     for names in list_of_names:
+#
+#         vocabulary = dict.fromkeys([names[0]], names)
+#         vocabulary = dict.update([names[0], names])
+#
+#     return vocabulary
+#
+# print(thesaurus(list_of_names))
+
 # 4. * (вместо задачи 3) Написать функцию thesaurus_adv(), принимающую в качестве аргументов строки в формате «Имя Фамилия» и возвращающую словарь, в котором ключи — первые буквы фамилий, а значения — словари, реализованные по схеме предыдущего задания и содержащие записи, в которых фамилия начинается с соответствующей буквы. Например:
 # >>>thesaurus_adv("Иван Сергеев", "Инна Серова", "Петр Алексеев", "Илья Иванов", "Анна Савельева")
 # {
@@ -48,3 +76,21 @@ print(num_translate('Eight'))
 #         Например:
 # >>> get_jokes(2)
 # ["лес завтра зеленый", "город вчера веселый"]
+
+import random
+
+nouns = ["автомобиль", "лес", "огонь", "город", "дом"]
+adverbs = ["сегодня", "вчера", "завтра", "позавчера", "ночью"]
+adjectives = ["веселый", "яркий", "зеленый", "утопичный", "мягкий"]
+my_list = []
+def get_jokes(n):
+    flag = 0
+    while flag != n:
+        my_list.append(nouns[random.randint(0, len(nouns)-1)] + ' ' + adverbs[random.randint(0, len(adverbs)-1)] \
+                                                                 + ' ' + adjectives[random.randint(0, len(adjectives)-1)])
+        flag += 1
+
+    return my_list
+
+print(get_jokes(12))
+
